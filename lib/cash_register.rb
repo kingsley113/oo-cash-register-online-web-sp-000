39579e7a65a8c9e6e7ecd0599e88e27983f1e6d0
Cameron Kingsley <cameron.kingsley13@gmail.com>
2020-08-04 23:41:17 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -3,8 +3,12 @@ require 'pry'
 class CashRegister
   attr_accessor :total
 
-  def initialize
+  def initialize(discount = false)
     @total = 0
+    @discount = discount
   end
+
+  def add_item(name, price)
+    
 
 end
